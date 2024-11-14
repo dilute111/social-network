e7629a058d6dbf6a9befdb9e6d0483c62f7ff18e
dilute111 <loshock666@gmail.com>
2024-11-15 01:28:24 +0500

    test

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Header";
+import Navbar from "./Navbar";
+import ProfilePage from "./ProfilePage";
+
+const App1 = () => {
+    return (
+
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <ProfilePage />
+
+        </div>
+    );
+}
