ceb368b70d911ae267e7ddd798e9d4d603ce311f
dilute111 <loshock666@gmail.com>
2024-11-19 20:15:24 +0500

    test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import classes from './Navbar.module.css';
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}><a href="#s">Profile</a></div>
-            <div className={classes.item}><a href="#s">Messages</a></div>
-            <div className={classes.item}><a href="#s">News</a></div>
-            <div className={classes.item}><a href="#s">Music</a></div>
-            <div className={classes.item}><a href="#s">Settings</a></div>
+            <div className={classes.item}><a href="/profile">Profile</a></div>
+            <div className={classes.item}><a href="/messages">Messages</a></div>
+            <div className={classes.item}><a href="/news">News</a></div>
+            <div className={classes.item}><a href="/music">Music</a></div>
+            <div className={classes.item}><a href="/settings">Settings</a></div>
         </nav>
     )
 }
